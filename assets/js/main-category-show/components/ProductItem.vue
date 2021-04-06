1171c8cb1a742d78c44fcb99ce05340cf3b37821
Thenostromo <alexandr.brook@sipuni.com>
2021-04-06 13:44:17 +0300

    Fix-1

--- assets/js/main-category-show/components/ProductItem.vue
+++ assets/js/main-category-show/components/ProductItem.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="col-6 col-md-4 col-lg-4 col-xl-3">
-    <div class="product-item text-center">
-      <div class="product-content">
-        <figure class="product-media">
-          <span class="product-sale-status sale-status-new">New</span>
-          <a :href="getUrlProductShow(product.id)">
-            <img
-                v-for="image in product.images"
-                :src="getUrlProductImage(product, image)"
-                :alt="product.title"
-                :class="product-image"
-            >
+    <div class="product-item">
+      <figure class="product-media">
+        <span class="product-sale-status sale-status-new">New</span>
+        <a :href="getUrlProductShow(product.id)">
+          <img
+              v-for="image in product.images"
+              :src="getUrlProductImage(product, image)"
+              :alt="product.title"
+              :class="product-image"
+          >
+        </a>
+
+        <div class="product-actions">
+          <a href="#" @click="addToCart(product)" class="btn-add-to-cart">
+            add to cart
           </a>
+        </div>
+      </figure>
 
-          <div class="product-actions">
-            <a href="#" @click="addToCart(product)" class="btn-add-to-cart">
-              add to cart
-            </a>
-          </div>
-        </figure>
-
-        <div class="product-body">
-          <h3 class="product-title">
-            <a :href="getUrlProductShow(product.id)">{{ product.title }}</a>
-          </h3>
-          <div class="product-price">
-            ${{ product.price }}
-          </div>
+      <div class="product-card-content">
+        <h3 class="product-title">
+          <a :href="getUrlProductShow(product.id)">{{ product.title }}</a>
+        </h3>
+        <div class="product-price justify-content-center">
+          ${{ product.price }}
         </div>
       </div>
     </div>
