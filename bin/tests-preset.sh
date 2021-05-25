87dc59fe1b7f140360e49a2d35646196bb9c7c6f
Alexander Brook <redlesleys@gmail.com>
2021-05-25 11:32:06 +0300

    Fixed

--- bin/tests-preset.sh
+++ bin/tests-preset.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-sudo apt install -y libzip-dev libpng-dev libpq-dev libxslt1.1 libxslt1-dev libnss3-tools --no-install-recommends
-sudo apt install php7.4 php7.4-zip php7.4-gd php7.4-pgsql php7.4-xsl php7.4-curl php7.4-mbstring php7.4-intl php7.4-fpm
+apt install -y libzip-dev libpng-dev libpq-dev libxslt1.1 libxslt1-dev libnss3-tools --no-install-recommends
+apt install php7.4 php7.4-zip php7.4-gd php7.4-pgsql php7.4-xsl php7.4-curl php7.4-mbstring php7.4-intl php7.4-fpm
 wget https://getcomposer.org/download/2.0.13/composer.phar
-sudo mv composer.phar /usr/bin/composer && chmod +x /usr/bin/composer
+mv composer.phar /usr/bin/composer && chmod +x /usr/bin/composer
 wget https://get.symfony.com/cli/installer -O - | sudo bash
-sudo mv ~/.symfony/bin/symfony /usr/local/bin/symfony
+mv ~/.symfony/bin/symfony /usr/local/bin/symfony
 composer install --no-dev --optimize-autoloader
